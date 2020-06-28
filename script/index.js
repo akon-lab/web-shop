5e5fb1cf42ba76ea98e57800ce1e486046d5f420
Akniet Adil <62028427+akon-lab@users.noreply.github.com>
2020-06-28 22:14:27 +0600

    first

---
+++ script/index.js
@@ -0,0 +1,21 @@
+function openNav() {
+    document.getElementById("Navka").style.width = '37%';
+}
+
+function closeNav() {
+    document.getElementById("Navka").style.width = "0";
+}
+
+window.onkeyup = okno; // нажатие Esc, см. условие "e.keyCode==27" 
+document.getElementById('popup').onclick = okno;
+
+function okno(e) {
+    if (e.target.nodeName != 'DIV' && e.target.nodeName != 'FIGCAPTION' || e.keyCode == 27) { // через && перечисляются теги, клинкув на которые окно не будет закрыто; сюда же можно включить тег A или IFRAME 
+        document.getElementById('popup').style.display = 'none';
+        localStorage.setItem('popup1', 'none');
+    }
+}
+
+if (localStorage.getItem('popup1')) {
+    document.getElementById('popup').style.display = 'none';
+}
